cfa0609700469d59df901d72d4cc500ecb0ccf08
sobored <maxim.balov19@gmail.com>
2025-04-08 16:11:40 +0200

    Добавлено новое заголовок X-New-Token в настройки CORS для улучшения взаимодействия с клиентом.

--- src/main.py
+++ src/main.py
@@ -51,6 +51,7 @@ app.add_middleware(
     allow_origins=settings.ALLOWED_ORIGINS,
     allow_methods=["POST", "GET", "PUT", "DELETE", "PATCH"],
     allow_headers=["*"],
+    expose_headers=["X-New-Token"],
 )
 
 def get_db():
