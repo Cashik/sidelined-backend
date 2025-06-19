3ffb5e8b42921042bde63ac53bbb4a442d69a77f
Alexandr <ooo647kn@gmail.com>
2025-06-19 20:18:41 +0300

    fix 3

--- src/services/redis_client.py
+++ src/services/redis_client.py
@@ -3,27 +3,8 @@ from src.config.settings import settings
 from urllib.parse import urlparse
 import ssl
 
-parsed_url = urlparse(settings.REDIS_URL)
-is_redis_ssl = parsed_url.scheme == 'rediss'
-
 redis_kwargs = {"decode_responses": True}
 
-if is_redis_ssl:
-    ssl_cert_reqs_map = {
-        'CERT_NONE': ssl.CERT_NONE,
-        'CERT_OPTIONAL': ssl.CERT_OPTIONAL,
-        'CERT_REQUIRED': ssl.CERT_REQUIRED,
-    }
-    cert_reqs = ssl_cert_reqs_map.get(settings.REDIS_SSL_CERT_REQS, ssl.CERT_NONE)
-    if cert_reqs == ssl.CERT_NONE:
-        # Для self-signed/Valkey: пустой контекст, без проверки
-        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        ssl_context.check_hostname = False
-        ssl_context.verify_mode = ssl.CERT_NONE
-    else:
-        ssl_context = ssl.create_default_context()
-        ssl_context.check_hostname = settings.REDIS_SSL_CHECK_HOSTNAME
-        ssl_context.verify_mode = cert_reqs
-    redis_kwargs["ssl"] = ssl_context
+url_without_params = settings.REDIS_URL.split("?")[0]
 
-redis_client = redis.from_url(settings.REDIS_URL, **redis_kwargs) 
+redis_client = redis.from_url(url_without_params, **redis_kwargs) 
